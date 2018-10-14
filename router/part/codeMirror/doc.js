02014288d9d61c77746899cb196f34ad8080c92b
softkaikai <282748212@qq.com>
2018-10-14 17:54:21 +0800

    update

--- router/part/codeMirror/doc.js
+++ router/part/codeMirror/doc.js
@@ -32,5 +32,55 @@ router.post('/add', async (req, res) => {
     }
 });
 
+router.post('/update', async (req, res) => {
+    try {
+        let body = req.body;
+        await dbApi.update(teamC, {_id: toId(body.id)}, {$set: {[`docs.${body.index}`]: body.doc}});
+        res.json({
+            code: '0',
+            msg: '更新成功',
+            data: null
+        })
+
+    } catch(err) {
+        let errMsg = '';
+        if (typeof err === 'string') {
+            errMsg = err;
+        } else {
+            errMsg = err.message;
+        }
+        res.json({
+            code: '500',
+            msg: errMsg,
+            data: null
+        });
+    }
+});
+
+router.post('/delete', async (req, res) => {
+    try {
+        let body = req.body;
+        await dbApi.update(teamC, {_id: toId(body.id)}, {$pull: {docs: {title: body.title}}});
+        res.json({
+            code: '0',
+            msg: '删除成功',
+            data: null
+        })
+
+    } catch(err) {
+        let errMsg = '';
+        if (typeof err === 'string') {
+            errMsg = err;
+        } else {
+            errMsg = err.message;
+        }
+        res.json({
+            code: '500',
+            msg: errMsg,
+            data: null
+        });
+    }
+});
+
 
 module.exports = router;
